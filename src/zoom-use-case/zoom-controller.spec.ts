6168131830ae741396f1e423151ef5d9b29c36b0
Lucas Machado <lucasmachado.rj@gmail.com>
2023-04-04 00:27:48 -0300

    [feature] should return zoom as 0.75 and cursorPositionInCanvas as (48, -67) when cursorPointer is in (40, 40) and cursorYOffset is negative

--- src/zoom-use-case/zoom-controller.spec.ts
+++ src/zoom-use-case/zoom-controller.spec.ts
@@ -52,4 +52,17 @@ describe('ZoomController execute method', () => {
     expect(zoomOutputDTO.zoom).toBe(1.25);
     expect(zoomOutputDTO.cursorPositionInCanvas).toEqual({ x: 32, y: 32 });
   });
+
+  it('should return zoom as 0.75 and cursorPositionInCanvas as (48, -67) when cursorPointer is in (40, 40) and cursorYOffset is negative', () => {
+    const cursorPointer = {
+      x: 48,
+      y: -50,
+    };
+    const cursorYOffset = -1;
+    const zoomOutputDTO = zoomController.execute(cursorPointer, cursorYOffset);
+
+    expect(zoomOutputDTO.zoom).toBe(0.75);
+    expect(zoomOutputDTO.cursorPositionInCanvas.x).toBeCloseTo(64);
+    expect(zoomOutputDTO.cursorPositionInCanvas.y).toBeCloseTo(-66.67);
+  });
 });
