2b6a045163381f1a014fa1bcf0df818b5bcf7ea5
Lucas Machado <lucasmachado.rj@gmail.com>
2023-04-04 00:35:29 -0300

    [feature] should return zoom as 1.5625 and cursorPositionInCanvas as (15.6, 15.6) when cursorPointer is in (40, 40), camera is positioned in (10, 10) and cursorYOffset is positive and initial zoom is 1.25

--- src/zoom-use-case/zoom-controller.spec.ts
+++ src/zoom-use-case/zoom-controller.spec.ts
@@ -79,4 +79,19 @@ describe('ZoomController execute method', () => {
     expect(zoomOutputDTO.zoom).toBe(1.25);
     expect(zoomOutputDTO.cursorPositionInCanvas).toEqual({ x: 22, y: 22 });
   });
+
+  it('should return zoom as 1.5625 and cursorPositionInCanvas as (15.6, 15.6) when cursorPointer is in (40, 40), camera is positioned in (10, 10) and cursorYOffset is positive and initial zoom is 1.25', () => {
+    fakeCamera.zoom = 1.25;
+    fakeCamera.x = 10;
+    fakeCamera.y = 10;
+    const cursorPointer = {
+      x: 40,
+      y: 40,
+    };
+    const cursorYOffset = 1;
+    const zoomOutputDTO = zoomController.execute(cursorPointer, cursorYOffset);
+    expect(zoomOutputDTO.zoom).toBe(1.5625);
+    expect(zoomOutputDTO.cursorPositionInCanvas.x).toBeCloseTo(15.6);
+    expect(zoomOutputDTO.cursorPositionInCanvas.y).toBeCloseTo(15.6);
+  });
 });
